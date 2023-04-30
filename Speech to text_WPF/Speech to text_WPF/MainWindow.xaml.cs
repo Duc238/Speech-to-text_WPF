49adaf52ab6f13a5f5af81ebc2371a46c0b3e1d7
Duc238 <92005265+Duc238@users.noreply.github.com>
2023-04-30 20:33:23 +0700

    Add files via upload

---
+++ Speech to text_WPF/Speech to text_WPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Speech_to_text_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SpeechSynthesizer speechSynthesizer=new SpeechSynthesizer();
+        PromptBuilder promptBuilder=new PromptBuilder();
+        SpeechRecognitionEngine speechRecognitionEngine=new SpeechRecognitionEngine();
+        Choices choices = new Choices();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            btnStart.IsEnabled = false;
+            btnStop.IsEnabled = true;
+            Grammar grammar=new DictationGrammar();
+            try
+            {
+                speechRecognitionEngine.RequestRecognizerUpdate();
+                speechRecognitionEngine.LoadGrammar(grammar);
+                speechRecognitionEngine.SpeechRecognized += SpeechRecognitionEngine_SpeechRecognized;
+                speechRecognitionEngine.SetInputToDefaultAudioDevice();
+                speechRecognitionEngine.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error");
+            }
+        }
+
+        private void SpeechRecognitionEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            txtText.Text=txtText.Text+e.Result.Text.ToString()+Environment.NewLine;
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            speechRecognitionEngine.RecognizeAsyncStop();
+            btnStart.IsEnabled=true;
+            btnStop.IsEnabled=false;
+        }
+    }
+}
